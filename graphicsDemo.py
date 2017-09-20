d5b5507cee45b15b1e74ed1a9ee307727c62ab90
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-20 13:54:12 -0400

    Updated from Brython Server: 9/20/2017 1:54:14 PM

--- graphicsDemo.py
+++ graphicsDemo.py
@@ -10,10 +10,20 @@ blue = Color(0x0000FF,1)
 black = Color(0x000000,1)
 white = Color(0xFFFFFF,1)
 
-blackOutline = LineStyle(1, black) #pixels, color
+blackOutline = LineStyle(10, black) #pixels, color
 
-redRectangle = RectangleAsset(200, 100, blackOutline, blue) #width, height, outline color, inside color
+blueRectangle = RectangleAsset(500, 200, blackOutline, blue) #width, height, outline color, inside color
+greenCircle = CircleAsset(100, blackOutline, green) #radius, outline, inside color
+redEllipse = EllipseAsset(100, 50, blackOutline, red) #horiz_radius, vertical_radius, outline, inside color
+blackLine = LineAsset(50, 100, blackOutline) #x_endpoint, y_endpoint, lineStyle
+redTriangle = PolygonAsset([(0,0), (120, 180), (60,300)], blackOutline, red) #list of endpoints
+text = TextAsset('Murph', fill=blue, style= 'bold 80pt Times')
 
-Sprite(redRectangle)
+Sprite(blueRectangle, (50, 50))
+Sprite(greenCircle, (900, 200)) #shape, right, left
+Sprite(redEllipse, (900, 400))
+Sprite(blackLine, (700, 100))
+Sprite(redTriangle, (600, 150))
+Sprite(text,(150, 350))
 
 App().run()
