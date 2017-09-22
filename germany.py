8fc38a784512888b3bca0c76400ec526ce429c54
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-22 14:26:43 -0400

    Updated from Brython Server: 9/22/2017 2:26:43 PM

--- germany.py
+++ germany.py
@@ -1,1 +1,22 @@
+#Emily Murphy
+#2017-09-22
+#germany.py - German flag
+
+from ggame import *
+
+black = Color(0x000000, 1)
+red = Color(0xFF0000,1)
+yellow = Color(0xFFFF00,1)
+
+blackOutline = LineStyle(5, black)
+
+blackRect = RectangleAsset(500, 100, blackOutline, black)
+redRect = RectangleAsset(500, 100, blackOutline, red)
+yellowRect= RectangleAsset(500, 100, blackOutline, yellow)
+
+Sprite(blackRect,(200,0))
+Sprite(redRect,(200,100))
+Sprite(yellowRect,(200,200))
+
+App().run()
 
