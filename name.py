5d7a74c4ad29947882aaeb9159e390e7548be4c9
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-22 14:45:46 -0400

    Updated from Brython Server: 9/22/2017 2:45:47 PM

--- name.py
+++ name.py
@@ -1,1 +1,18 @@
+#Emily Murphy
+#2017-09-20
+#name.py - input name and an RGB color code --> print name in middle and background color is RGB input
 
+from ggame import *
+blue = Color(0x0000FF,1)
+name = input('Enter your name: ')
+background = Color(input('Enter RGB code color: '), 1)
+outline = LineStyle(10, background)
+
+rectangle = RectangleAsset(1000, 1000, outline, background)
+text = TextAsset(name, fill= blue, style= '40pt Times')
+
+
+Sprite(rectangle)
+Sprite(text,(450,250))
+
+App().run()
