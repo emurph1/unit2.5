d6e8a2a29846d564de585d94fd05ef84709a45a7
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-26 14:35:46 -0400

    Updated from Brython Server: 9/26/2017 2:35:47 PM

--- yield.py
+++ yield.py
@@ -6,14 +6,18 @@ from ggame import *
 
 red = Color(0xFF0000,1)
 white = Color(0xFFFFFF,1)
+black = Color(0x000000,1)
 
 whiteOutline = LineStyle(5, white)
-yieldTri = PolygonAsset([(6,0),(100,-150), (293, 0)], whiteOutline, white)
-yieldTri2 = PolygonAsset([(6,0),(150,250), (293, 0)], whiteOutline, red)
+blackOutline = LineStyle(10, black)
+yieldTri = PolygonAsset([(55,50),(150,190), (245, 50)], whiteOutline, white)
+yieldTri2 = PolygonAsset([(5,10),(150,250), (295, 10)], whiteOutline, red)
+text = TextAsset('YIELD', fill=red, style= 'bold 20pt Times')
 
 
 Sprite(yieldTri2, (200, 200))
-Sprite(yieldfTri, (200,200))
+Sprite(yieldTri, (200,200))
+Sprite(text, (310,250))
 
 App().run()
 
