9d9921d5a832d55b4266024a4cefa2c34e06d1b3
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-26 14:25:40 -0400

    Updated from Brython Server: 9/26/2017 2:25:41 PM

--- yield.py
+++ yield.py
@@ -8,11 +8,12 @@ red = Color(0xFF0000,1)
 white = Color(0xFFFFFF,1)
 
 whiteOutline = LineStyle(5, white)
-yieldTri = PolygonAsset([(6,0),(150,-150), (293, 0)], whiteOutline, white)
-yieldTri2 = PolygonAsset([(6,0),(150,-150), (293, 0)], whiteOutline, red)
+yieldTri = PolygonAsset([(6,0),(100,-150), (293, 0)], whiteOutline, white)
+yieldTri2 = PolygonAsset([(6,0),(150,250), (293, 0)], whiteOutline, red)
 
-Sprite(yieldTri, (200, 200))
+
 Sprite(yieldTri2, (200, 200))
+Sprite(yieldfTri, (200,200))
 
 App().run()
 
